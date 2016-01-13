17ac36364f403550b8afab317bcc163e7d6e6c0b
Uy Nguyễn <leesan.itus@gmail.com>
2016-01-13 22:29:33 +0700

    Complete v1.0

--- count-sku.js
+++ count-sku.js
@@ -1,1 +1,25 @@
+var myArgs = process.argv; //Get arguments from user
 
+if(myArgs.length < 3){
+	console.log("The number of argument is invalid !");
+	return;
+}
+var inSku = myArgs[2];
+
+// Read Synchrously
+var fs = require("fs");
+var contents = fs.readFileSync("orders.json");
+var data = JSON.parse(contents);
+
+var result = 0;
+
+for(var key in data.rows) {
+	var order = data.rows[key].doc.order_items;
+	for(var index_order_item in order){
+	 	if(order[index_order_item].sku == inSku){
+	 		result++;
+	 	}
+	}
+}
+
+console.log(result + " times");
