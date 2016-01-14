7c359f648075266b53d742af5df85cfdae07b842
Uy Nguyễn <leesan.itus@gmail.com>
2016-01-14 16:20:32 +0700

    Add try catch & fix count mistake

--- count-sku.js
+++ count-sku.js
@@ -1,25 +1,31 @@
-var myArgs = process.argv; //Get arguments from user
+try{
+	var myArgs = process.argv; //Get arguments from user
 
-if(myArgs.length < 3){
-	console.log("The number of argument is invalid !");
-	return;
-}
-var inSku = myArgs[2];
-
-// Read Synchrously
-var fs = require("fs");
-var contents = fs.readFileSync("orders.json");
-var data = JSON.parse(contents);
-
-var result = 0;
-
-for(var key in data.rows) {
-	var order = data.rows[key].doc.order_items;
-	for(var index_order_item in order){
-	 	if(order[index_order_item].sku == inSku){
-	 		result++;
-	 	}
+	if(myArgs.length < 3){
+		console.log("The number of argument is invalid !");
+		return;
 	}
-}
+	var inSku = myArgs[2];
 
-console.log(result + " times");
+	// Read Synchrously
+	var fs = require("fs");
+	var contents = fs.readFileSync("orders.json");
+	var data = JSON.parse(contents);
+
+	var result = 0;
+
+	for(var key in data.rows) {
+		var order = data.rows[key].doc.order_items;
+		for(var index_order_item in order){
+		 	if(order[index_order_item].sku == inSku){
+		 		result += parseInt(order[index_order_item].qty_ordered);
+		 	}
+		}
+	}
+
+	console.log(result + " times");
+
+}
+catch(err){
+	console.log("An error occurs: " + err);
+}
